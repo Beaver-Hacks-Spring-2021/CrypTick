f1d2e69ad5e9311d21e1071c9b46eaad41351997
Nick DiMeglio <contact@nickdimeglio.com>
2021-04-09 23:06:02 -0400

    Pricing for BTC, ETH, LTC from Messari

--- coins.js
+++ coins.js
@@ -1,21 +1,33 @@
 // BTC 
-const btcPrice = document.querySelector("#btc-price");
-const btc24hChange = document.querySelector("#btc-24h-change");
+window
+  .fetch("https://data.messari.io/api/v1/assets/btc/metrics/market-data")
+  .then(res => res.json())
+  .then(messariRes => messariRes.data)
+  .then(
+    payload =>
+      (document.getElementById("current-btc-price").innerHTML =
+        '$' + payload.market_data.price_usd.toLocaleString())
+  );
 
 // ETH
-const ethPrice = document.querySelector("#eth-price");
-const eth24hChange = document.querySelector("#eth-24h-change");
+window
+  .fetch("https://data.messari.io/api/v1/assets/eth/metrics/market-data")
+  .then(res => res.json())
+  .then(messariRes => messariRes.data)
+  .then(
+    payload =>
+      (document.getElementById("current-eth-price").innerHTML =
+        '$' + payload.market_data.price_usd.toLocaleString())
+  );
 
 // Litecoin
-const litePrice = document.querySelector("#lite-price");
-const lite24hChange = document.querySelector("#lite-24h-change");
+window
+  .fetch("https://data.messari.io/api/v1/assets/ltc/metrics/market-data")
+  .then(res => res.json())
+  .then(messariRes => messariRes.data)
+  .then(
+    payload =>
+      (document.getElementById("current-ltc-price").innerHTML =
+        '$' + payload.market_data.price_usd.toLocaleString())
+  );
 
-
-btcPrice.innerHTML = 60000;
-btc24hChange.innerHTML = 150;
-
-ethPrice.innerHTML = 'a price'
-eth24hChange.innerHTML = 'some change'
-
-litePrice.innerHTML = .02
-lite24hChange.innerHTML = 10
