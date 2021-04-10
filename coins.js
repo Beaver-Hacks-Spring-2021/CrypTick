6015c5dd2865d544e44b313cb26a67d935d6f6bc
Nick DiMeglio <contact@nickdimeglio.com>
2021-04-09 23:28:58 -0400

    More info from Messari API

--- coins.js
+++ coins.js
@@ -4,9 +4,13 @@ window
   .then(res => res.json())
   .then(messariRes => messariRes.data)
   .then(
-    payload =>
-      (document.getElementById("current-btc-price").innerHTML =
-        '$' + payload.market_data.price_usd.toLocaleString())
+    payload => {
+        document.getElementById("current-btc-price").innerHTML =
+        '$' + payload.market_data.price_usd.toLocaleString();
+
+        percent_change = parseFloat(payload.market_data.percent_change_usd_last_24_hours.toLocaleString()).toFixed(2);
+        document.getElementById("btc-24h-change").innerHTML = percent_change + '%';
+    }
   );
 
 // ETH
@@ -15,9 +19,13 @@ window
   .then(res => res.json())
   .then(messariRes => messariRes.data)
   .then(
-    payload =>
-      (document.getElementById("current-eth-price").innerHTML =
-        '$' + payload.market_data.price_usd.toLocaleString())
+    payload => {
+        document.getElementById("current-eth-price").innerHTML =
+        '$' + payload.market_data.price_usd.toLocaleString();
+
+        percent_change = parseFloat(payload.market_data.percent_change_usd_last_24_hours.toLocaleString()).toFixed(2);
+        document.getElementById("eth-24h-change").innerHTML = percent_change + '%';
+    }
   );
 
 // Litecoin
@@ -26,8 +34,12 @@ window
   .then(res => res.json())
   .then(messariRes => messariRes.data)
   .then(
-    payload =>
-      (document.getElementById("current-ltc-price").innerHTML =
-        '$' + payload.market_data.price_usd.toLocaleString())
+    payload => {
+        document.getElementById("current-ltc-price").innerHTML =
+        '$' + payload.market_data.price_usd.toLocaleString();
+
+        percent_change = parseFloat(payload.market_data.percent_change_usd_last_24_hours.toLocaleString()).toFixed(2);
+        document.getElementById("ltc-24h-change").innerHTML = percent_change + '%';
+    }
   );
 
