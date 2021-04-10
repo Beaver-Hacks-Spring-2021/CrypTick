730c1ab4967de2b732413eab4a9bad3667c74fd1
Sean Park <chanseanpark@gmail.com>
2021-04-09 23:35:02 -0700

    file changes, latest

--- news.js
+++ news.js
@@ -1,7 +1,7 @@
-const url =
-  'https://cryptopanic.com//api/v1/posts/?auth_token=9de2bfa6bd699f1fdb53c95acd6485c0edb23643&public=true';
-
-fetch(url)
+window
+  .fetch(
+    'https://cryptopanic.com//api/v1/posts/?auth_token=9de2bfa6bd699f1fdb53c95acd6485c0edb23643&public=true'
+  )
   .then((data) => {
     return data.json();
   })
