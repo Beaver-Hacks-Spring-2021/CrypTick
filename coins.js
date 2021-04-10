d8ed8a4b8c554a0d75a7d05a138054474a33a87f
Nick DiMeglio <contact@nickdimeglio.com>
2021-04-09 22:30:48 -0400

    fake prices!

---
+++ coins.js
@@ -0,0 +1,21 @@
+// BTC 
+const btcPrice = document.querySelector("#btc-price");
+const btc24hChange = document.querySelector("#btc-24h-change");
+
+// ETH
+const ethPrice = document.querySelector("#eth-price");
+const eth24hChange = document.querySelector("#eth-24h-change");
+
+// Litecoin
+const litePrice = document.querySelector("#lite-price");
+const lite24hChange = document.querySelector("#lite-24h-change");
+
+
+btcPrice.innerHTML = 60000;
+btc24hChange.innerHTML = 150;
+
+ethPrice.innerHTML = 'a price'
+eth24hChange.innerHTML = 'some change'
+
+litePrice.innerHTML = .02
+lite24hChange.innerHTML = 10
