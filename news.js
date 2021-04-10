72805b53d3a9786d96edc4b00ee5bfa91c020bba
Sean Park <chanseanpark@gmail.com>
2021-04-09 21:41:38 -0700

    commiting local changes before merge

--- news.js
+++ news.js
@@ -1,32 +1,10 @@
-document.querySelector('.changeData').addEventListener('click', changeData);
+const url =
+  'https://cryptopanic.com//api/v1/posts/?auth_token=9de2bfa6bd699f1fdb53c95acd6485c0edb23643&public=true';
 
-const getDataAndRender = (function getData() {
-  const xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'https://data.messari.io/api/v1/news', true);
-
-  let out = JSON.stringify(output);
-
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      const dataArr = response.Data;
-      dataArr.forEach(function (data) {
-        const name = data.CoinInfo.FullName;
-        const imgURL = data.CoinInfo.ImageUrl;
-        const price = data.DISPLAY.INR.PRICE;
-        console.log(typeof data.CoinInfo.FullName);
-        const img = `https://www.cryptocompare.com/${imgURL}`;
-        const highDay = data.DISPLAY.INR.HIGHDAY;
-        const lowDay = data.DISPLAY.INR.LOWDAY;
-        output += ` <tr><td><img src=${img} width="30" height="30"></td>
-            <td>${name}</td>
-            <td>${price}</td>
-            <td>${highDay}</td>
-            <td>${lowDay}</td></tr>`;
-      });
-      document.querySelector('.data-back').innerHTML = output;
-    }
-  };
-  xhr.send();
-})();
+fetch(url)
+  .then((data) => {
+    return data.json();
+  })
+  .then((res) => {
+    console.log(res);
+  });
